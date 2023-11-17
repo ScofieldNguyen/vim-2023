416c766caba9f207bc9e4f630850c6bdab9f0bc9
Tien Nguyen <scofieldnguyen96@gmail.com>
2023-11-17 23:08:23 +0700

    v1.0.0

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,64 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+local remap = { remap = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+--
+
+-- Delete a word backwards
+keymap.set("n", "dw", "wb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- buffers
+keymap.set("n", "<Right>", ":BufferLineCycleNext<Return>", opts)
+keymap.set("n", "<Left>", ":BufferLineCyclePrev<Return>", opts)
+keymap.set("n", ",D", "<leader>bo", { remap = true }) -- delete others buffer
+
+-- Split windows
+keymap.set("n", "<C-x>", ":split<Return>", opts)
+keymap.set("n", "<C-v>", ":vsplit<Return>", opts)
+
+-- Dianostics
+keymap.set("n", "]c", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+keymap.set("n", "[c", function()
+  vim.diagnostic.goto_prev()
+end, opts)
+
+-- Code action
+keymap.set("n", "<leader>n", "<leader>ca", remap)
+
+-- Source action
+keymap.set("n", "<leader>N", "<leader>cA", remap)
+
+-- Rename
+keymap.set("n", "<leader>rn", "<leader>cr", remap)
+
+-- Find files
+keymap.set("n", "<C-p>", "<leader><space>", remap)
+
+-- Search global
+keymap.set("n", "<C-S-f>", "<leader>/", remap)
+
+-- Recent files
+keymap.set("n", "<C-e>", "<leader>fr", remap)
+
+-- Surround
+keymap.set("v", "S", "gsa", remap)
+keymap.set("n", "dS", "gsd", remap)
+keymap.set("n", "rS", "gsd", remap)
+
+-- open tree
+keymap.set("n", "<C-n>", "<space>e", remap)
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,4 @@
+return {
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+}
