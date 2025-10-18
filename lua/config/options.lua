b9fab098bf174adb272a0cc3b5017b8d7f054e81
scofieldnguyen <scofieldnguyen96@gmail.com>
2025-10-18 17:37:41 +0700

    light background

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,3 +24,5 @@ vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true }
 vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+vim.o.background = "light"
